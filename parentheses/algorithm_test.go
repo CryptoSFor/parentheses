eb50b1ea1a525d597742f58c41226f2807585f0f
Vladimir Ivanov <drpodt.ivv@gmail.com>
2021-11-30 16:52:54 +0300

    Remove unused test and update test case

--- parentheses/algorithm_test.go
+++ parentheses/algorithm_test.go
@@ -4,57 +4,6 @@ import (
 	"testing"
 )
 
-func Test_pairOfParentheses(t *testing.T) {
-	type args struct {
-		x1 string
-		x2 string
-	}
-
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		{
-			name: "comparison of } and {",
-			args: args{x1: "{", x2: "}"},
-			want: true,
-		},
-		{
-			name: "comparison of ) and (",
-			args: args{x1: "(", x2: ")"},
-			want: true,
-		},
-		{
-			name: "comparison of ] and [",
-			args: args{x1: "[", x2: "]"},
-			want: true,
-		},
-		{
-			name: "comparison of { and }",
-			args: args{x1: "}", x2: "{"},
-			want: false,
-		},
-		{
-			name: "comparison of empty string and [",
-			args: args{x1: "", x2: "]"},
-			want: false,
-		},
-		{
-			name: "comparison of empty string and empty string",
-			args: args{x1: "", x2: ""},
-			want: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := pairOfParentheses(tt.args.x1, tt.args.x2); got != tt.want {
-				t.Errorf("isEqual() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestBalancedString(t *testing.T) {
 	tests := []struct {
 		name string
@@ -68,7 +17,7 @@ func TestBalancedString(t *testing.T) {
 		},
 		{
 			name: "balanced2",
-			s:    "效{[复]制___()+{仿效_}}制",
+			s:    "(те[с]т){}",
 			want: true,
 		},
 		{
