75d091ff6b73490d1ceb02a969c5f8f26ddd1e0a
Vladimir Ivanov <drpodt.ivv@gmail.com>
2021-12-10 16:23:25 +0300

    Refactor test cases

--- service/service_test.go
+++ service/service_test.go
@@ -13,39 +13,26 @@ func TestParenthesesGeneration(t *testing.T) {
 		name       string
 		length     int
 		wantLength int
-		wantErr    error
 	}{
 		{
 			name:       "length:1",
 			length:     1,
 			wantLength: 1,
-			wantErr:    nil,
 		},
 		{
 			name:       "length:100",
 			length:     100,
 			wantLength: 100,
-			wantErr:    nil,
 		},
 		{
 			name:       "length:0",
 			length:     0,
 			wantLength: 0,
-			wantErr:    nil,
-		},
-		{
-			name:       "length:-1",
-			length:     -1,
-			wantLength: 0,
-			wantErr:    ErrNegativeLength,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := ParenthesesGeneration(tt.length)
-			if err != tt.wantErr {
-				t.Errorf("ParenthesesGeneration() error = %v, wantErr %v", err, tt.wantErr)
-			}
+			got := ParenthesesGeneration(tt.length)
 			if len(got) != tt.wantLength {
 				t.Errorf("ParenthesesGeneration() = %v, want %v", got, tt.wantLength)
 			}
@@ -73,7 +60,7 @@ func TestGenerateHandler(t *testing.T) {
 			httpStatus: 200,
 		},
 		{
-			name:       "len of the sequence : 1-",
+			name:       "len of the sequence : -1",
 			length:     -1,
 			expLength:  0,
 			httpStatus: 400,
